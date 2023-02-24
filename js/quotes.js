4e3dba8aebd41d464674f608cd1d3268acbfbcff
topurayhan007 <topurayhan007@gmail.com>
2023-02-24 18:50:29 +0600

    added all practice

--- js/quotes.js
+++ js/quotes.js
@@ -6,6 +6,16 @@ const loadQuote = () => {
     .then((data) => displayQuote(data));
 };
 
+const loadQuote2 = async () => {
+  try {
+    const res = await fetch("https://api.kanye.rest");
+    const data = await res.json();
+    displayQuote(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 const displayQuote = (quote) => {
   const blockQuote = document.getElementById("quote");
   blockQuote.innerHTML = quote.quote;
